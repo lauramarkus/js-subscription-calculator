2b4c6bebf9cfede50e02ff69b3d687b47526474b
Laura Markus <41956880+lauramarkus@users.noreply.github.com>
2021-11-23 12:52:29 +0100

    adds global variables and change function, updates img location

--- js/script.js
+++ js/script.js
@@ -1,1 +1,11 @@
 // Your JS code here
+const subTypeElement = document.querySelector("#subscription");
+const subDurationElement = document.querySelector("#months");
+const result = document.querySelector(".result");
+var subType = "basic";
+var subDuration = 1;
+
+subTypeElement.addEventListener("change", function (e){
+    subType = e.target.value;
+    console.log(subType);
+});
