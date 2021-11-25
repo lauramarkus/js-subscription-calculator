9e16afa4fd67e72e936fff1141251828ac79ad0c
Laura Markus <41956880+lauramarkus@users.noreply.github.com>
2021-11-25 17:56:33 +0100

    adds calculation function

--- js/script.js
+++ js/script.js
@@ -7,5 +7,23 @@ var subDuration = 1;
 
 subTypeElement.addEventListener("change", function (e){
     subType = e.target.value;
-    console.log(subType);
+    //console.log(subType);
+    updateSubscriptionDiv();
 });
+
+subDurationElement.addEventListener("change", function (e){
+    subDuration = Number (e.target.value);
+    //console.log(subDuration);
+    updateSubscriptionDiv();
+});
+
+const updateSubscriptionDiv = function(){
+    let monthlyCost = 5;
+    if (subType === "standard"){
+        monthlyCost = 7;
+    } else if (subType === "premium"){
+        monthlyCost = 10;
+    }
+    const total = (subDuration * monthlyCost);
+    result.innerText=`You have chosen a ${subType} plan for a total of $${total}`;
+};
